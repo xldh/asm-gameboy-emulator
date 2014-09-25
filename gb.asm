66aad1fe2b311fb60f33dadc20592eb64aebd313
Axel Duch <axlduch@gmail.com>
2014-09-25 16:02:01 +0200

    Add gb.asm (main file)

---
+++ gb.asm
@@ -0,0 +1,22 @@
+global _start
+
+section .bss
+    %include "cpu.inc.asm"
+
+section .text
+
+    _start:
+        %include "memory_fill.inc.asm"
+        
+        ; test
+        test_mmu_rb:
+            mov edi, 0xFFFF ; address to read from for rb
+            call rb
+    exit:
+        mov rdi, 1
+        mov rax, 3ch
+        syscall
+
+; function includes
+%include "mmu.inc.asm"
+
