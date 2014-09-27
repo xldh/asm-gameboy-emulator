170c5dd8cb805fce455341e3c059bdf97edf322c
Axel Duch <axlduch@gmail.com>
2014-09-27 15:23:08 +0200

    Flag register added in cpu states

--- cpu/states.inc.asm
+++ cpu/states.inc.asm
@@ -1,9 +1,11 @@
     memory_map resb 0x10000         ; memory from 0x0 to 0xFFFF
 
-    _AF  resb 2                      ; 8 bit registers
-    _BC  resb 2
-    _DE  resb 2
-    _HL  resb 2
+    _AF resb 2                      ; 16 bits registers addressable by 8 bits too
+    _BC resb 2
+    _DE resb 2
+    _HL resb 2
+    _FLAG resb 1                    ; flag register
 
     _PC resw 1                      ; 16 bit registers
     _SP resw 1
+
