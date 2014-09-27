61aed13a1ba38a389de9c7612b77b50b81cd4d60
Axel Duch <axlduch@gmail.com>
2014-09-27 17:33:00 +0200

    add rbp and rsp parts to functions

--- mmu.inc.asm
+++ mmu.inc.asm
@@ -3,11 +3,27 @@
 ; READ BYTE AT [EDI]
 ; rdi (clamped to edi here) holds the address to read byte from
 rb:
+    push    rbp
+    mov     rbp, rsp
+
+    ; ENTRY POINT
     mov     al, BYTE [memory_map + edi]
+
+    ; LEAVE
+    mov     rsp, rbp
+    pop     rbp
     ret
 
 ; READ WORD ad [EDI]
 ; rdi (clamped to edi here too) holds the address to read word from
 rw:
+    push    rbp
+    mov     rbp, rsp
+    ; ENTER
+
     mov     ax, WORD [memory_map + edi]
+
+    ; LEAVE
+    mov     rsp, rbp
+    pop     rbp
     ret
