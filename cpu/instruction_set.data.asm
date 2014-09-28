c7b0dad5852eb7acd7904ce477008dcb2bf2b4f4
Axel Duch <axlduch@gmail.com>
2014-09-28 20:18:28 +0200

    Update instruction struct and instruction set

--- cpu/instruction_set.data.asm
+++ cpu/instruction_set.data.asm
@@ -1,7 +1,8 @@
-nop:
-    istruc instruction
-        at instruction.info,         db 0x00
-        at instruction.size,         db 0x00
-        at instruction.ascii,        db 'nop', 0
-        at instruction.function_ptr, dd op_nop
-    iend
+instruction_set:
+    nop:
+        istruc instruction
+            at instruction.info,         db 0x00
+            at instruction.op_size,      db 0x00
+            at instruction.ascii,        db 'nop', 0
+            at instruction.function_ptr, dd op_nop
+        iend
--- instruction.struct.asm
+++ instruction.struct.asm
@@ -1,7 +1,7 @@
 struc instruction   ; Instruction instance is 32 bits
 
     .info:           resb 8     ; bytecode, 
-    .size:           resd 1
+    .op_size:        resd 1
     .ascii:          resb 16
     .function_ptr:   resd 1     ; size of an address is 8 octets (64 bits)
 
